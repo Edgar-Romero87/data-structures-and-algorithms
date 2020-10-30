123e2c846ae78038d44422500f4bc08833cd9c96
EDGAR ROMERO <EDGAROMERO1987@GMAIL.COM>
2020-10-29 17:49:22 -0700

    finished code challenge 12

---
+++ data-structures/fifoAnimalShelter/__test__/animal-shelter.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const AS = require('../fifo-animal-shelter.js');
+
+
+describe('FIFO animal shelter tests', () => {
+  it('should be a class', () => {
+    expect(AS).toBeDefined();
+  });
+
+});
